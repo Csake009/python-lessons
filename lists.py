6acec07283b4b1e30943e8743b629a5f59d56d96
Chris Sakellaris <chris.angel.1900@outlook.com.gr>
2021-03-21 22:12:33 +0200

    lists

---
+++ lists.py
@@ -0,0 +1,5 @@
+names = ["chris", "angel", "george", "voula"]
+for name in names:
+    print(name)
+for name in names[1:]:
+    print(name)   
---
+++ lists2.py
@@ -0,0 +1,8 @@
+mynames = ["chris", "sakellaris"]
+#for name in mynames:
+#print(f"My name is {mynames[0]}")
+mynames.pop(1)
+print(mynames)
+for n in range(1,101):
+    print(f"{n} {mynames[0]}")
+    
