f5c474a1947e916e90bdf818562976dfcd47110b
Chris Sakellaris <chris.angel.1900@outlook.com.gr>
2021-04-05 21:39:57 +0300

    first program

--- exercise.py
+++ exercise.py
@@ -1,28 +1,37 @@
 names = []
 degrees = []
 max_degree = 0
-for i in range(2):
-    name = input("Δώσε μου το όνοματα του μαθητή: ")
+number_of_studens = input("Πες μου πόσοι είναι οι μαθητές: ")
+number_of_studens = int(number_of_studens)
+
+# το πρόγραμμα ζητάει από τον χρήστη να του δώσει το όνομα του μαθητή και τον βαθμό του
+for i in range(number_of_studens):
+    name = input(f"{i + 1}. Δώσε μου το όνομα του μαθητή: ")
     names.append(name)
-    degree = input("Τώρα πες μου τon βαθμό του: ")
-    degrees.append(int(degree))    
+    degree = input(f"{i + 1}. Τώρα πες μου τον βαθμό του: ")
+    # αποθηκεύω τον βαθμό σαν αριθμό κινητής υποδιαστολής
+    degrees.append(float(degree))    
 
 #for u in range(2):
 #    print(f"{names[u]} {degrees[u]}")
 
+# η εντολή max επιστρέφει τον μεγαλήτερο αριθμό της λίστας
 max_degree = max(degrees)
 print(f"Ο μεγαλήτερος βαθμός που πήρε μαθητής είναι {max_degree}")
 
-
-for y in range(2):
+# σαρώνω τη λίστα και βρίσκω τους μαθητές με τον μεγαλήτερο αριθμό
+for y in range(number_of_studens):
     if degrees[y] == max_degree:
         print(f"ο/η {names[y]} πήρε: {degrees[y]}")
 
+average_degree = 0
+# σαρώνω τη λίστα για να βρώ τον μέσο όρο
+for u in range(number_of_studens):
+    average_degree = average_degree + degrees[u]
+average_degree = average_degree / len(degrees)
 
-
+print(f"Ο μέσος όρος των βαθμών είναι {average_degree}")
 
 
 #max_index = degrees.index(max_degree)
 #print(max_index)
-
-
